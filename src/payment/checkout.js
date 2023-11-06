765a03a935aa67c7fb4c9026c57eee3e32fe922f
mist3rW <wannarak.nutt@gmail.com>
2023-11-06 23:05:17 +0700

    finish seed.js

--- src/payment/checkout.js
+++ src/payment/checkout.js
@@ -1,15 +1,13 @@
-const stripeAPI = require("./stripe");
+const stripeAPI = require('./stripe');
 
 // สร้าง session สำหรับการเช่า เมื่อ user กด rent now
 const createCheckoutSession = async (req, res) => {
   const domainUrl = process.env.FIND_WEB_APP_URL;
-  console.log(" ##### req.body#####", req.body);
+  console.log(' ##### req.body#####', req.body);
   const { line_items, customer_email } = req.body;
   // เช็คว่ามี line_items และ customer email จาก req body ไหม
   if (!line_items || !customer_email) {
-    return res
-      .status(400)
-      .json({ error: "missing required session parameters" });
+    return res.status(400).json({ error: 'missing required session parameters' });
   }
 
   let session;
@@ -17,16 +15,20 @@ const createCheckoutSession = async (req, res) => {
   try {
     // สร้าง session อิงกับ doc ของ Stripe ต้องส่งค่าต่างๆที่ required
     session = await stripeAPI.checkout.sessions.create({
-      payment_method_types: ["card"],
-      mode: "payment",
+      payment_method_types: ['card'],
+      mode: 'payment',
       line_items,
       customer_email,
       success_url: `${domainUrl}/success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `${domainUrl}/cancelled`,
+      cancel_url: `${domainUrl}/cancelled`
     });
+    if (session) {
+      prisma.rent.create;
+    }
+
     res.status(200).json({ sessionId: session.id });
   } catch (error) {
-    console.error("Error", error);
+    console.error('Error', error);
     console.log(error);
   }
 };
