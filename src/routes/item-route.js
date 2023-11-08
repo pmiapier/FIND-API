0b7e5f25916331f5bdebe545383ec4dbe9f7a6ae
PlumeSC <supawit.chukiat@gmail.com>
2023-11-08 11:12:25 +0700

    myRentalAndRented90percent

--- src/routes/item-route.js
+++ src/routes/item-route.js
@@ -1,4 +1,5 @@
-const { getAllItem, getSingleItem,getCategories } = require("../controllers/item-controller")
+const { getAllItem, getSingleItem,getCategories,myRentalItem,myRentedItem } = require("../controllers/item-controller")
+const authenticate = require("../middlewares/authenticate")
 
 const router = require(`express`).Router()
 
@@ -8,6 +9,9 @@ const router = require(`express`).Router()
 router.get(`/`,getAllItem)
 router.post(`/get-single-item`,getSingleItem)
 router.get(`/getCategories`,getCategories)
+
+router.get(`/myRentalItem`,authenticate,myRentalItem)
+router.get(`/myRentedItem`,authenticate,myRentedItem)
 
 
 
