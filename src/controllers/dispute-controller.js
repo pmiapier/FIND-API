1dd1f74d14de12a17d0812d24e0589433c7f707e
pmiapier <pmiapier@proton.me>
2023-11-12 09:41:36 +0400

    init dispute

---
+++ src/controllers/dispute-controller.js
@@ -0,0 +1,14 @@
+const prisma = require(`../models/prisma`)
+
+// open dispute
+// resolve dispute
+// escalate dispute (to admin)
+// respond dispute
+
+
+const openDispute = async (req, res, next) => {
+
+}
+
+
+module.exports = {openDispute}
