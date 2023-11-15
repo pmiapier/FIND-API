205221b1de8078d7e03c610b55531950860fc9c1
WacharinCh <wacharin1504@gmail.com>
2023-11-16 04:07:36 +0700

    chat can be used

--- src/socket-io.js
+++ src/socket-io.js
@@ -86,7 +86,7 @@ module.exports = (io) => {
                     type
                 }
             })
-            console.log("send_message from :", authUser.id, "to:", onlineUser[+data.to].userId);
+            // console.log("send_message from :", authUser.id, "to:", onlineUser[+data.to].userId);
             io.to(onlineUser[data.to]?.socketId).emit(`receive_message`, data)
 
         })
